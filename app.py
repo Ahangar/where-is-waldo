f2d183946e1ccfaaf2b352d5ca9051f9cbeda7f3
Faraz <faraz.e.ahangar@gmail.com>
2026-02-08 06:44:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 from ultralytics import YOLO
+import cv2
+
 
 @st.cache_resource
 def load_model():
@@ -16,7 +18,17 @@ if uploaded:
     image = Image.open(uploaded).convert("RGB")
     st.image(image, caption="Input")
 
+    # Annotated image with bounding boxes
+
     results = model(image)
     annotated = results[0].plot()
+
+    annotated = cv2.cvtColor(annotated, cv2.COLOR_BGR2RGB)  # convert to RGB
+
+    # Display in Jupyter
+    plt.figure(figsize=(10,10))
+    plt.imshow(annotated)
+    plt.axis("off")
+    plt.show()
 
     st.image(annotated, caption="Detected")
